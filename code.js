7d96ded4f193903d0f13e0ac2d7805ab2af29173
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2024-10-08 13:58:58 -0400

    stuff

--- code.js
+++ code.js
@@ -4,11 +4,15 @@ const students = [
   { name: "Charlie", age: 21, grade: 78 },
   { name: "Diana", age: 23, grade: 95 },
 ];
-
-// 1. Log each student’s name and grade
-
+// 1
 students.forEach((student) => console.log(student.name, ", ", student.grade));
+// 2
 const topStudents = students.filter((student) => student.grade > 80);
-console.log(topStudents);
 const youngStudents = students.filter((student) => student.age < 21);
-console.log(youngStudents);
+// 3
+const topLog = topStudents.forEach((topStudent) =>
+  console.log(topStudent.name)
+);
+const youngLog = youngStudents.forEach((youngStudent) =>
+  console.log(youngStudent.name)
+);
